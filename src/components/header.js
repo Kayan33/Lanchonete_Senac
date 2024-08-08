5cafcc0e39be2227f43f07f556c4aa72d83dc283
Kayan Pereira <138801461+Kayan33@users.noreply.github.com>
2024-08-08 17:20:18 -0300

    ajustes modallogar

--- src/components/header.js
+++ src/components/header.js
@@ -1,40 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style/global.css";
 import "../style/header.css";
 import logo from '../img/logoShekina 1.png';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping, faUser,} from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import { faCartShopping, faUser } from "@fortawesome/free-solid-svg-icons";
+import ModalLogar from '../components/modalLogar'; 
 
 function Header() {
-    return (
-        <div className="container">
-            <img src={logo} alt="Logo Shekina" />
-            <ul className="flex-container">
-                <li className="flex-item">
-                    <Link to="Cadastrar">
-                    Baixe APP
-                    </Link>
-                </li>
-                <li className="flex-item">
-                    <Link to="Cadastrar">
-                    Cupom
-                    </Link>
-                </li>
-            </ul>
-            <ul className="flex-container">
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-                <li className="flex-item">
-                    <FontAwesomeIcon icon={faCartShopping} size="xl" style={{ color: "#D5F5F9" }} />
-                </li>
-                <li className="flex-item">
-                    <Link to="modal">
-                        Cadastrar
-                    </Link>
-                    <FontAwesomeIcon icon={faUser} size="xl" />
-                </li>
-            </ul>
-        </div>
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
+
+    return (
+        <>
+            <div className="container">
+                <img src={logo} alt="Logo Shekina" />
+                <ul className="flex-container">
+                    <li className="flex-item">
+                        <a href="#!">Baixe APP</a>
+                    </li>
+                    <li className="flex-item">
+                        <a href="#!">Cupom</a>
+                    </li>
+                </ul>
+                <ul className="flex-container">
+                    <li className="flex-item">
+                        <FontAwesomeIcon icon={faCartShopping} size="xl" style={{ color: "#D5F5F9" }} />
+                    </li>
+                    <li className="flex-item">
+                        <a href="#!" onClick={openModal} className="logar">
+                            logar-me
+                        </a>
+                        
+                    </li>
+                </ul>
+            </div>
+
+            {isModalOpen && (
+                <ModalLogar closeModal={closeModal} />
+            )}
+        </>
     );
 }
 
--- src/components/modalLogar.js
+++ src/components/modalLogar.js
@@ -2,30 +2,101 @@ import React, { useState } from "react";
 import "../style/global.css";
 import "../style/modalLogar.css";
 
-function ModalLogar() {
+function ModalLogar({ closeModal }) {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [cpf, setCpf] = useState('');
+    const [birthdate, setBirthdate] = useState('');
+    const [phone, setPhone] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
     const [isLogin, setIsLogin] = useState(true);
 
     const toggleForm = () => {
         setIsLogin(!isLogin);
     };
 
+    const EnviaDados = (e) => {
+        e.preventDefault();
+        console.log(email, senha, cpf, birthdate, phone, confirmPassword);
+    }
+
     return (
-        <div className="modal-container">
-            <div className="modal-content">
+        <div className="modal-container" onClick={closeModal}>
+            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                <button className="close-button" onClick={closeModal}>X</button>
                 <h2>{isLogin ? "Login" : "Cadastro"}</h2>
-                <form>
-                    <div className="form-group">
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" name="email" required />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Senha:</label>
-                        <input type="password" id="password" name="password" required />
+                <form onSubmit={EnviaDados}>
+                    <div className={`form-group ${!isLogin ? "two-columns" : ""}`}>
+                        <div>
+                            <label htmlFor="email">Email:</label>
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="password">Senha:</label>
+                            <input
+                                type="password"
+                                id="password"
+                                name="password"
+                                value={senha}
+                                onChange={(e) => setSenha(e.target.value)}
+                                required
+                            />
+                        </div>
                     </div>
                     {!isLogin && (
-                        <div className="form-group">
-                            <label htmlFor="confirmPassword">Confirmar Senha:</label>
-                            <input type="password" id="confirmPassword" name="confirmPassword" required />
+                        <div className="form-group two-columns">
+                            <div>
+                                <label htmlFor="confirmPassword">Confirmar Senha:</label>
+                                <input
+                                    type="password"
+                                    id="confirmPassword"
+                                    name="confirmPassword"
+                                    value={confirmPassword}
+                                    onChange={(e) => setConfirmPassword(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="cpf">CPF:</label>
+                                <input
+                                    type="text"
+                                    id="cpf"
+                                    name="cpf"
+                                    value={cpf}
+                                    onChange={(e) => setCpf(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="birthdate">Data de Aniversário:</label>
+                                <input
+                                    type="date"
+                                    id="birthdate"
+                                    name="birthdate"
+                                    value={birthdate}
+                                    onChange={(e) => setBirthdate(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="phone">Celular:</label>
+                                <input
+                                    type="tel"
+                                    id="phone"
+                                    name="phone"
+                                    value={phone}
+                                    onChange={(e) => setPhone(e.target.value)}
+                                    required
+                                />
+                            </div>
                         </div>
                     )}
                     <button type="submit">{isLogin ? "Entrar" : "Cadastrar"}</button>
@@ -39,4 +110,3 @@ function ModalLogar() {
 }
 
 export default ModalLogar;
-
